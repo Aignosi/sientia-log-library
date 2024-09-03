7b7c84461138bba6bcbdeb9e91772cf65ef1c316
Matheus Demoner <matheus@aignosi.com>
2024-09-03 10:10:51 -0300

    chore: Remove unnecessary imports

--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
 from setuptools import setup, find_packages
 
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+# with open("requirements.txt", "r") as req_file:
+#     requirements = req_file.read().splitlines()
 
 setup(
     name='sientia_tracker',
-    version='1.0.2',
-    author=['Ítalo Azevedo', 'Pedro Bahia'],
-    author_email=['italo@aignosi.com.br', 'pedro.bahia@aignosi.com.br'],
+    version='1.0.6',
+    license='Apache License 2.0',
+    author=['Ítalo Azevedo', 'Pedro Bahia', 'Matheus Demoner'],
+    author_email=['italo@aignosi.com.br', 'pedro.bahia@aignosi.com.br', 'matheus@aignosi.com.br'],
     description='Library for Aignosi Tracking API',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    keywords='sientia',
     packages=['sientia_tracker'],
-    install_requires=requirements,
+    install_requires=['mlflow==2.10.1', 'typing'],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
-
-
     ],
 )
+
